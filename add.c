52810f0688479b208b6deb677583dcb5c92faf09
Yohanna Abana <yohanaabana02@gmail.com>
2023-08-30 17:10:37 +0100

    fix error

--- add.c
+++ add.c
@@ -10,6 +10,8 @@
 */
 void add(stack_t **stack, unsigned int line_number)
 {
+    stack_t *temp = NULL;
+
     if (*stack == NULL)
     {
         fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
@@ -20,5 +22,8 @@ void add(stack_t **stack, unsigned int line_number)
         fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
     }
-    printf("%d\n", (*stack)->n + (*stack)->next->n);
+    (*stack)->next->n = (*stack)->n + (*stack)->next->n;
+    temp = *stack;
+    *stack = (*stack)->next;
+    free(temp);
 }
