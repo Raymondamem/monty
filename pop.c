ba529ba4e5d4cdbed5d86764b3f776b104458220
Yohanna Abana <yohanaabana02@gmail.com>
2023-08-30 16:49:56 +0100

    fix segmentation fault

--- pop.c
+++ pop.c
@@ -10,15 +10,18 @@
 */
 void pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *temp = NULL;
+	stack_t *temp = NULL;
 
-    if (*stack == NULL)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    temp = *stack;
-    (*stack)->next->prev = NULL;
-    *stack = (*stack)->next;
-    free(temp);
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(temp);
 }
